7cbd78955dbc97df9e7c7a3466e0245af14eba93
MagnusAleksandar <alevel.col@gmail.com>
2026-05-18 20:47:57 -0500

    Agregadas data classes del model

---
+++ app/src/main/java/com/example/vaquita/model/Goal.kt
@@ -0,0 +1,7 @@
+package com.example.vaquita.model
+
+import android.media.Image
+import java.time.LocalDate
+
+data class Goal(var name: String, var description: String, var dueDate: LocalDate, var contributions: MutableList<Contribution>,
+                var image: Image)
---
+++ app/src/main/java/com/example/vaquita/model/Person.kt
@@ -0,0 +1,3 @@
+package com.example.vaquita.model
+
+data class Person(val name: String, val id: String)
