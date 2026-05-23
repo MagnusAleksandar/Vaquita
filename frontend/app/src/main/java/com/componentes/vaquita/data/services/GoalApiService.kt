f93eb5546cda589a4f461ee978812aa3e5d0efbe
MagnusAleksandar <alevel.col@gmail.com>
2026-05-23 16:47:42 -0500

    Creada API de meta

---
+++ frontend/app/src/main/java/com/componentes/vaquita/data/services/GoalApiService.kt
@@ -0,0 +1,42 @@
+package com.componentes.vaquita.data.services
+
+import com.example.vaquita.model.Goal
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface GoalApiService {
+    // Constantes
+    companion object{
+        const val BASE = "api/goals"
+        const val ID = "mongoId"
+        const val TEMPLATE = "$BASE/{id}"
+    }
+
+    // Crea meta
+    @POST(BASE)
+    suspend fun createGoal(@Body goal: Goal): Response<Goal>
+
+    // Obtiene una meta
+    @GET(TEMPLATE)
+    suspend fun getOneGoal(@Path(ID) id: String): Response<Goal>
+
+    // Obtiene todas las metas
+    @GET(BASE)
+    suspend fun getAllGoals(): Response<Goal>
+
+    // Actualiza meta
+    @PUT(TEMPLATE)
+    suspend fun updateGoal(
+        @Path(ID) id: String,
+        @Body goal: Goal
+    ): Response<Goal>
+
+    // Elimina meta
+    @DELETE(TEMPLATE)
+    suspend fun deleteGoal(@Path(ID) id: String): Response<Goal>
+}
